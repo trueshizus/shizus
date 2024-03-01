aafc384139630967f3865be0f16033e1012a72af
Shizus <4212982+trueshizus@users.noreply.github.com>
2024-03-01 14:26:26 +0000

    Change the order of the posts

--- components/posts.tsx
+++ components/posts.tsx
@@ -1,29 +1,28 @@
 import fs from "fs";
 import path from "path";
 
-// Specify the directory you want to list the files from
 const directoryPath = path.join("./entries");
 
 const getPost = async () => {
-  let post: JSX.Element[] = [];
+  let posts: JSX.Element[] = [];
   try {
-    // Synchronously read the directory
     const files = fs
       .readdirSync(directoryPath)
       .filter((filename) => filename.endsWith(".mdx"));
-    files.forEach(async (file) => {
+
+    for (const file of files) {
       const { default: Component } = await import(`@/entries/${file}`);
-      post.push(
+      posts.push(
         <article key={file}>
           <Component />
         </article>
       );
-    });
+    }
   } catch (err) {
     console.log("Unable to scan directory: " + err);
   }
 
-  return post;
+  return posts.reverse();
 };
 
 export default async function Posts() {
