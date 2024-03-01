76b5f3014119e9f9cfdce9730cee5df8aab98178
Shizus <4212982+trueshizus@users.noreply.github.com>
2024-03-01 23:03:05 +0000

    Adjust <p>

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -3,6 +3,7 @@ import type { MDXComponents } from "mdx/types";
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     ...components,
-    h1: ({ children }) => <h1 className="text-3xl mb-4">{children}</h1>,
+    h1: ({ children }) => <h1 className="text-3xl mb-2">{children}</h1>,
+    p: ({ children }) => <p className="text-lg mb-1">{children}</p>,
   };
 }
