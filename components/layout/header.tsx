3a215571746a3db9a3934498f675238aeb8e2000
Shizus <jesus.herrera.90@gmail.com>
2025-01-24 12:04:49 +0100

    Add blinking cursor animation to header logs link and update styles in globals.css

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <header className="flex justify-between items-center my-2">
       <Link href="/" className="text-2xl font-bold">shizus.dev</Link>
-      <Link href="/logs">Logs</Link>
+      <Link href="/logs">logs: <span className="cursor_blink">_</span></Link>
     </header>
   );
 }
