add7c89805dd03a4dd24224f066e483f3e8b8880
trueshizus <jesus.herrera.90@gmail.com>
2025-01-28 17:02:04 +0100

    Add blinking cursor and link to logs page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
 import CV from "@/public/cv.mdx";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <article className="h-full text-zinc-200 p-4">
-      
         <div className="border border-zinc-200 max-w-4xl mx-auto">
           <div className="flex items-center justify-between bg-zinc-200">
             <p className=" text-zinc-900 px-2 font-mono text-sm">cv</p>
-            <p className="text-zinc-200 text-sm">2025</p>
+            <Link href="/logs" className="text-zinc-900 pr-2 text-sm blink">_</Link>
           </div>
           <div className="px-4 py-2">
             <CV />
