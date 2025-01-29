3a36488b0ce608bb2f8ee85492f91930653c7e29
trueshizus <jesus.herrera.90@gmail.com>
2025-01-29 03:13:03 +0100

    Adjust Post component padding for improved layout

--- components/post.tsx
+++ components/post.tsx
@@ -5,7 +5,7 @@ type Props = {
 
 export default async function Post({ filename, content }: Props) {
   return (
-    <article key={filename} className="md:px-8 md:py-2 px-4 py-4 border-b border-zinc-200">
+    <article key={filename} className="px-4 py-2 border-b border-zinc-200">
       <small className="text-zinc-500 text-xs">$ cat ./{filename}</small>
       {content}
     </article>
