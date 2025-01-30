ddcff6846972699cfb155bfd10a04c9c83d17036
trueshizus <jesus.herrera.90@gmail.com>
2025-01-30 18:43:54 +0100

    Update Model Basics project content and page layout

--- app/projects/model-basic/page.tsx
+++ app/projects/model-basic/page.tsx
@@ -5,7 +5,9 @@ export default function Page() {
   return (
     <main className="h-full md:py-12 py-4 px-2">
       <Terminal title="1. Model Basics" href="/">
-        <Content />
+        <article className="p-4">
+          <Content />
+        </article>
       </Terminal>
     </main>
   );
