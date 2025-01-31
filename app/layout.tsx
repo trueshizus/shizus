0e4a109ec64ffdea0efe4b937ce071805e40e94e
trueshizus <jesus.herrera.90@gmail.com>
2025-01-31 02:01:44 +0100

    Add draggable functionality to Terminal component

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={spaceMono.className}>
-      <body className="h-full bg-zinc-900 min-w-[400px]">
+      <body className="h-screen bg-zinc-900 min-w-[400px]">
         {children}
 
         <Analytics />
--- components/terminal.tsx
+++ components/terminal.tsx
@@ -1,4 +1,8 @@
+'use client';
+
 import Link from "next/link";
+import Draggable from "react-draggable";
+import { useRef } from "react";
 
 type Props = {
   children: React.ReactNode;
@@ -8,17 +12,20 @@ type Props = {
 };
 
 export default function Terminal({ children, title, href, className }: Props) {
+  const nodeRef = useRef<HTMLDivElement | null>(null);
   return (
-    <div className={`h-full text-zinc-200 ${className}`}>
-      <div className="border border-zinc-200 max-w-4xl mx-auto">
-        <div className="flex items-center justify-between bg-zinc-200">
-          <p className=" text-zinc-900 px-2 font-mono text-sm">{title}</p>
-          <Link href={href} className="text-zinc-900 pr-2 text-sm blink">
-            _
-          </Link>
+    <Draggable handle=".handle" nodeRef={nodeRef}>
+      <div ref={nodeRef} className={`h-full text-zinc-200 ${className}`}>
+        <div className="border border-zinc-200 max-w-4xl mx-auto">
+          <div className="flex items-center justify-between bg-zinc-200 handle cursor-move">
+            <p className="text-zinc-900 px-2 font-mono text-sm">{title}</p>
+            <Link href={href} className="text-zinc-900 pr-2 text-sm">
+              x
+            </Link>
+          </div>
+          {children}
         </div>
-        {children}
       </div>
-    </div>
+    </Draggable>
   );
 }
