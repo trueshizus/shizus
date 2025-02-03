3060cd5ffe995d88cd8ff09b08084efe9600b9cf
trueshizus <jesus.herrera.90@gmail.com>
2025-02-03 18:39:26 +0100

    Add dynamic routing for logs and unlisted pages

---
+++ app/unlisted/[slug]/page.tsx
@@ -0,0 +1,25 @@
+import Terminal from "@/components/terminal";
+import fs from "fs";
+import path from "path";
+
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) {
+  const slug = (await params).slug;
+  const { default: Entry } = await import(`@/app/unlisted/${slug}.mdx`);
+
+  return (
+    <Terminal title={slug} href="/unlisted">
+      <Entry />
+    </Terminal>
+  );
+}
+
+export function generateStaticParams() {
+  const slugs = fs.readdirSync(path.join(process.cwd(), "app/unlisted"));
+  return slugs.map((slug) => ({ slug: slug.replace(".mdx", "") }));
+}
+
+export const dynamicParams = false;
