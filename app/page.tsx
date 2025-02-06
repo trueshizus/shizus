d3c6296ca7cf6e7c6a310ae27a40f76f58b29fd8
trueshizus <jesus.herrera.90@gmail.com>
2025-02-06 16:44:50 +0100

    Hide settings link on mobile screens

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
               href="/menu"
               aria-label="Settings"
               title="Settings"
-              className="flex items-center justify-center w-4 h-4 text-zinc-900 text-xs  hover:bg-zinc-500"
+              className="items-center justify-center w-4 h-4 text-zinc-900 text-xs  hover:bg-zinc-500 hidden md:flex"
             >
               ⚙
             </Link>
