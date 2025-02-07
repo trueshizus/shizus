cc6d54dc02619026c2dae36d859d647c281a3bc2
trueshizus <jesus.herrera.90@gmail.com>
2025-02-07 16:00:43 +0100

    Add font selection context and update layout

--- types/html2pdf.d.ts
+++ types/html2pdf.d.ts
@@ -1,4 +1,21 @@
 declare module 'html2pdf.js' {
-    const html2pdf: any;
+    interface Html2PdfOptions {
+      filename?: string;
+      image?: {
+        type?: string;
+        quality?: number;
+      };
+      html2canvas?: {
+        scale?: number;
+      };
+      jsPDF?: {
+        format?: string;
+        unit?: string;
+        margin?: number[];
+        compress?: boolean;
+      };
+    }
+  
+    function html2pdf(element: HTMLElement, options?: Html2PdfOptions): Promise<void>;
     export default html2pdf;
-} 
+  }
