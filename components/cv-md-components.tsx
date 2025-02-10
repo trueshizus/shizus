480cd423fb77791fdfc1b64dd842565989f3fc82
trueshizus <jesus.herrera.90@gmail.com>
2025-02-10 02:58:14 +0100

    Fix lint

--- components/cv-md-components.tsx
+++ components/cv-md-components.tsx
@@ -1,5 +1,5 @@
 import { FontSizes } from "@/contexts/settings-context";
-import { FC, memo } from "react";
+import { FC } from "react";
 import type { Components } from "react-markdown";
 
 interface HeadingProps extends React.HTMLAttributes<HTMLHeadingElement> {}
@@ -7,45 +7,45 @@ interface ParagraphProps extends React.HTMLAttributes<HTMLParagraphElement> {}
 interface ListItemProps extends React.HTMLAttributes<HTMLLIElement> {}
 interface UnorderedListProps extends React.HTMLAttributes<HTMLUListElement> {}
 
-const H1: FC<HeadingProps> = memo(({ children, ...props }) => (
+const H1: FC<HeadingProps> = ({ children, ...props }) => (
   <h1 className="font-bold " {...props}>
     {children}
   </h1>
-));
+);
 
-const H2: FC<HeadingProps> = memo(({ children, ...props }) => (
+const H2: FC<HeadingProps> = ({ children, ...props }) => (
   <h2 className="font-bold  border-b-2 border-zinc-400 my-2" {...props}>
     {children}
   </h2>
-));
+);
 
-const H3: FC<HeadingProps> = memo(({ children, ...props }) => (
+const H3: FC<HeadingProps> = ({ children, ...props }) => (
   <h3 className="font-bold mt-3" {...props}>
     {children}
   </h3>
-));
+);
 
-const P: FC<ParagraphProps> = memo(({ children, ...props }) => (
+const P: FC<ParagraphProps> = ({ children, ...props }) => (
   <p className="my-1" {...props}>
     {children}
   </p>
-));
+);
 
-const Li: FC<ListItemProps> = memo(({ children, ...props }) => (
+const Li: FC<ListItemProps> = ({ children, ...props }) => (
   <li className="my-1" {...props}>
     {children}
   </li>
-));
+);
 
-const Ul: FC<UnorderedListProps> = memo(({ children, ...props }) => (
+const Ul: FC<UnorderedListProps> = ({ children, ...props }) => (
   <ul className="list-disc list-inside leading-relaxed mb-2" {...props}>
     {children}
   </ul>
-));
+);
 
-const Em: FC<ListItemProps> = memo(({ children, ...props }) => (
+const Em: FC<ListItemProps> = ({ children, ...props }) => (
   <small {...props}>{children}</small>
-));
+);
 
 export const cvComponents = (fontSizes: FontSizes): Components => ({
   h1: (props) => (
