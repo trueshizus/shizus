40f277e60cb3255197417054e4c12f552adef997
trueshizus <jesus.herrera.90@gmail.com>
2025-02-14 16:33:30 +0100

    Refactor useFontSizes hook to use individual query states

--- hooks/useFontSizes.ts
+++ hooks/useFontSizes.ts
@@ -1,12 +1,32 @@
 import { defaultFontSizes } from "@/lib/fonts";
-
-import { FontSizes } from "@/lib/fonts";
-import { useQueryState } from "nuqs";
+import { useQueryStates } from "nuqs";
 
 export default function useFontSizes() {
-  return useQueryState("fontSizes", {
-    defaultValue: defaultFontSizes,
-    parse: (value: string) => JSON.parse(value) as FontSizes,
-    serialize: (value: FontSizes) => JSON.stringify(value),
+  return useQueryStates({
+    h1: {
+      defaultValue: defaultFontSizes.h1,
+      parse: Number,
+      serialize: String,
+    },
+    h2: {
+      defaultValue: defaultFontSizes.h2,
+      parse: Number,
+      serialize: String,
+    },
+    h3: {
+      defaultValue: defaultFontSizes.h3,
+      parse: Number,
+      serialize: String,
+    },
+    p: {
+      defaultValue: defaultFontSizes.p,
+      parse: Number,
+      serialize: String,
+    },
+    li: {
+      defaultValue: defaultFontSizes.li,
+      parse: Number,
+      serialize: String,
+    },
   });
 }
