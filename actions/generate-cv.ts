20f81be142f13aaafbbf55164e29a614376c7529
trueshizus <jesus.herrera.90@gmail.com>
2025-05-28 12:56:25 +0200

    Update models

--- actions/generate-cv.ts
+++ actions/generate-cv.ts
@@ -13,8 +13,8 @@ import { createStreamableValue } from "ai/rsc";
 
 const modelConfig = {
   openai: openai("gpt-4o-mini"),
-  google: google("gemini-2.0-flash-exp"),
-  deepseek: deepseek("deepseek-v3"),
+  google: google("gemini-2.0-flash-lite"),
+  deepseek: deepseek("deepseek-chat"),
 } as const;
 
 export async function generate(model: AiProvider, style: StyleOptions) {
